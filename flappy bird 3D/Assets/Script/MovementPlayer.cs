31f12e553085c0c27698e7815c7b256c24bc1982
Tyo Hanan <60575547+tyohanan@users.noreply.github.com>
2021-12-13 14:17:11 +0700

    freelook camera cinemachine and test mechanics third person games

---
+++ flappy bird 3D/Assets/Script/MovementPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPlayer : MonoBehaviour
+{
+    Rigidbody rb;
+    public Vector3 Force;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(Force, ForceMode.Impulse);
+            //transform.Translate(Force);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(Force * -1, ForceMode.Impulse);
+
+            //transform.Translate(Force*-1);
+        }
+    }
+}
