c15cabb04d3fb1b37b36680fe70c6a9916475a46
Tyo Hanan <60575547+tyohanan@users.noreply.github.com>
2021-12-15 13:00:10 +0700

    unity move to 2020.3 export build fixed

---
+++ flappy bird 3D/Assets/Script/MovementBird.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBird : MonoBehaviour
+{
+    Rigidbody rb;
+    public Vector3 vectorForce;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(vectorForce, ForceMode.Impulse);
+        }
+    }
+}
