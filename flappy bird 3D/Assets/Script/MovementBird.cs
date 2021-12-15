0cf2310f10b52380a91af6d8426d2ccd3e27594a
Tyo Hanan <60575547+tyohanan@users.noreply.github.com>
2021-12-15 22:38:11 +0700

    test

--- flappy bird 3D/Assets/Script/MovementBird.cs
+++ flappy bird 3D/Assets/Script/MovementBird.cs
@@ -5,16 +5,41 @@ using UnityEngine;
 public class MovementBird : MonoBehaviour
 {
     Rigidbody rb;
-    public Vector3 vectorForce;
+    public Vector3 movementSpeed;
+    public float jumpForce;
 
     private void Start() {
         rb = GetComponent<Rigidbody>();
     }
 
     private void Update() {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            rb.AddForce(vectorForce, ForceMode.Impulse);
+        // if (Input.GetKeyDown(KeyCode.LeftArrow)){
+        //     rb.AddForce(vectorForce, ForceMode.Impulse);
+        // }
+        // else if (Input.GetKeyDown(KeyCode.RightArrow)){
+        //     rb.AddForce(vectorForce*-1, ForceMode.Impulse);
+        // }
+
+
+        //for moving left, right, up
+        if (Input.GetKeyDown(KeyCode.LeftArrow)){
+            transform.Translate(movementSpeed);
         }
+        else if (Input.GetKeyDown(KeyCode.RightArrow)){
+            transform.Translate(movementSpeed*-1);
+        }
+        else if (Input.GetKey(KeyCode.Space)){
+            rb.velocity += Vector3.up*jumpForce;
+        }
+
+        //for adding rotation when up or down
+        // print(rb.velocity.y);
+        // if (rb.velocity.y >= 0){
+        //     transform.Rotate(new Vector3(0,0, 45));
+        // }
+    }
+
+    private void FixedUpdate() {
+
     }
 }
