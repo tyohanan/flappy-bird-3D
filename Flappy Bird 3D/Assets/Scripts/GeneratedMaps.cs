6056a64dd90ee8f3d861d04428d0e3ca9128d22f
Tyo Hanan <60575547+tyohanan@users.noreply.github.com>
2021-12-20 01:45:52 +0700

    top platform generator maps done

--- Flappy Bird 3D/Assets/Scripts/GeneratedMaps.cs
+++ Flappy Bird 3D/Assets/Scripts/GeneratedMaps.cs
@@ -6,6 +6,10 @@ public class GeneratedMaps : MonoBehaviour
 {
     [SerializeField]
     private GameObject bird;
+    [SerializeField]
+    private int DistanceValue = 10;
+    [SerializeField]
+    private int safeDistanceValue = 8;
 
     public GameObject[] PlatformTop = new GameObject[3];
 
@@ -22,8 +26,8 @@ public class GeneratedMaps : MonoBehaviour
     {
         //generate awal saat player masuk
         birdPosition = bird.GetComponent<Transform>();   
-        minDistance = (int)birdPosition.position.x -10;
-        maxDistance = (int)birdPosition.position.x +10;
+        minDistance = (int)birdPosition.position.x -DistanceValue;
+        maxDistance = (int)birdPosition.position.x +DistanceValue;
         for (int x = minDistance; x <= maxDistance; x+=2){
             int getPlatform = Random.Range(0, PlatformTop.Length);
             int randomRotation = Random.Range(0,rotationDegree.Length);
@@ -34,17 +38,37 @@ public class GeneratedMaps : MonoBehaviour
     }
 
     void Update()
-    {
+{
+        //randomize platform and rotation
+        int getPlatform = Random.Range(0, PlatformTop.Length);
+        int randomRotation = Random.Range(0,rotationDegree.Length);
         //generate platform atas baru saat bird masuk ke safe distance
-        int safeDistance = (int)birdPosition.position.x + 8;
-        if (safeDistance >= maxDistance){
-            var safePlatform = Instantiate(PlatformTop[0], new Vector3(maxDistance+2,6,0), Quaternion.Euler(rotationDegree[0],90,0));
-            maxDistance = (int)birdPosition.position.x + 10;
+        int maxSafeDistance = (int) birdPosition.position.x + safeDistanceValue;
+        int minSafeDistance = (int) birdPosition.position.x - safeDistanceValue;
+        //check distance front
+        if (maxSafeDistance >= maxDistance){
+            var safePlatform = Instantiate(PlatformTop[getPlatform], new Vector3(maxDistance+2,6,0), Quaternion.Euler(rotationDegree[randomRotation],90,0));
+            maxDistance = (int)birdPosition.position.x + DistanceValue;
+            minDistance = (int)birdPosition.position.x - DistanceValue;
             platformList.Add(safePlatform);
-            Destroy(platformList[0]);
-            platformList.RemoveAt(0);
+
+
             // Debug.Log(maxDistance +" "+safeDistance+" "+(int)birdPosition.position.x);
         } 
+        //check distance back
+        else if (minSafeDistance <= minDistance){
+            var safePlatform = Instantiate(PlatformTop[getPlatform], new Vector3(minDistance-2,6,0), Quaternion.Euler(rotationDegree[randomRotation],90,0));
+            maxDistance = (int)birdPosition.position.x + DistanceValue;
+            minDistance = (int)birdPosition.position.x - DistanceValue;
+            platformList.Add(safePlatform);
+        }
+        //checking if the platform out of distance then we can destroy it
+        for (int x = 0; x < platformList.Count; x++){
+            if (platformList[x].transform.position.x < minDistance || platformList[x].transform.position.x > maxDistance){
+                Destroy(platformList[x]);
+                platformList.RemoveAt(x);
+            }
+        }
         Debug.Log(platformList.Count);
     }
 }
