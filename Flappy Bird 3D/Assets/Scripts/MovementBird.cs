b7d9545e4adf6cd9df5f91294af961ecac97c85c
Tyo Hanan <60575547+tyohanan@users.noreply.github.com>
2021-12-20 20:15:47 +0700

    Fixing bird controller system

--- Flappy Bird 3D/Assets/Scripts/MovementBird.cs
+++ Flappy Bird 3D/Assets/Scripts/MovementBird.cs
@@ -10,36 +10,41 @@ public class MovementBird : MonoBehaviour
     private float minSpeed;
     [SerializeField]
     private float maxSpeed;
+
     private float speedTranslate;
 
     public float forceRotate;
     private float Yvelocity;
     private float ZrotationPos;
 
+    public GameObject fire;
+
     private void Start() {
         rb = GetComponent<Rigidbody>();
         speedTranslate = minSpeed;
+        fire.GetComponent<ParticleSystem>().Stop(true);
     }
-
+    private float startGas = 1f;
     private void Update() {
-        // Debug.Log(speedTranslate);
         transform.Translate(speedTranslate*Time.deltaTime,0,0);
         if (Input.GetKey(KeyCode.UpArrow) && speedTranslate < maxSpeed){
             transform.Translate(speedTranslate*Time.deltaTime,0,0);
             speedTranslate++;
+            fire.GetComponent<ParticleSystem>().Play(true);
             }
         else if(speedTranslate > minSpeed){
             speedTranslate--;
         }
-        Yvelocity = rb.velocity.y;
-        ZrotationPos = transform.rotation.z;
-
-        if (Input.GetKey(KeyCode.LeftArrow)){
+        else if (Input.GetKey(KeyCode.LeftArrow)){
             rb.AddRelativeTorque(0,0,forceRotate*Time.deltaTime,ForceMode.Impulse);
+            fire.GetComponent<ParticleSystem>().Play(true);
         }
         else if (Input.GetKey(KeyCode.RightArrow)){
             rb.AddRelativeTorque(0,0, -forceRotate*Time.deltaTime, ForceMode.Impulse);
-
+            fire.GetComponent<ParticleSystem>().Play(true);
+        }
+        else{
+            fire.GetComponent<ParticleSystem>().Stop(true);
         }
     }
 
